64c7bc9fd5c41706bf99d623320347cf95c410a1
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-09-20 17:10:04 +0200

    remove unnecessary code

--- app/components/podcast-player.js
+++ app/components/podcast-player.js
@@ -4,15 +4,8 @@ export default Ember.Component.extend({
   didReceiveAttrs() {
     this._super(...arguments);
     let episode =  this.get('episode');
-    console.log(episode);
     let deadEnd = (episode.get('children').get('length') < 1);
     this.set('deadEnd', deadEnd);
-    if (!deadEnd) {
-      let children = episode.get('children');
-      let nextEpisode = children.objectAt(0);
-      console.log(nextEpisode);
-      this.set('nextEpisode', nextEpisode);
-    }
   },
   didRender(){
     let episode = this.get('episode');
