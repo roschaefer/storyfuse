c6f0436273ee2f5435f5671a297115e6f74d9d0c
Robert Schaefer <robert.schaefer@student.hpi.de>
2016-09-21 02:18:41 +0200

    using breadcrumbs for ancestors

--- app/components/podcast-player.js
+++ app/components/podcast-player.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: 'podcast-player',
+  ancestors: [],
   didReceiveAttrs() {
     this._super(...arguments);
     let episode =  this.get('episode');
@@ -12,11 +13,22 @@ export default Ember.Component.extend({
     if (parent.get('id')) {
       let alternatives = parent.get('children').reject((a) => {
         return a === episode;
-      });;
+      });
       let firstAlternative = alternatives.objectAt(0);
       let secondAlternative = alternatives.objectAt(1);
       this.set('firstAlternative', firstAlternative);
       this.set('secondAlternative', secondAlternative);
+
+      this.set('ancestors', []);
+      let ancestorParent = parent;
+      while(ancestorParent.get('id')){
+        this.get('ancestors').addObject(ancestorParent);
+        ancestorParent = ancestorParent.get('parent');
+      }
+      this.get('ancestors').reverse();
+      console.log(this.get('ancestors'));
+
     }
+
   }
 });
